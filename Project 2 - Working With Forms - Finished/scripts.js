fe897842358f4c4e8d91fabb9dc0134b22c9f637
oscarlmjr <oscarlmjr@gmail.com>
2026-08-26 20:30:14 -0300

    secao_1 aula_7-12

--- Project 2 - Working With Forms - Finished/scripts.js
+++ Project 2 - Working With Forms - Finished/scripts.js
@@ -1,15 +1,29 @@
-
 members = [
     {
-        fname: 'John',
-        lname: 'Lennon',
-        instrument: 'Violão'
+        fname: 'David',
+        lname: 'Loca',
+        instrument: 'Guitarra'
     },
     {
-        fname: 'George',
-        lname: 'Harrison',
+        fname: 'Wally',
+        lname: 'Byers',
+        instrument: 'Baixo'
+    },
+    {
+        fname: 'Robert',
+        lname: 'Dozal',
         instrument: 'Guitarra'
-    }
+    },
+    {
+        fname: 'Billy',
+        lname: 'Trujillo',
+        instrument: 'Teclado'
+    },
+    // {
+    //     fname: 'Tony',
+    //     lname: 'Leal',
+    //     instrument: 'Bateria'
+    // }
 ]
 
 const handlingForms = {
@@ -18,22 +32,24 @@ const handlingForms = {
         return {
             members: window.members,
             newMember: {
-
             }
         }
     },
 
     methods: {
         addMember: function() {
-            if (!(this.newMember.instrument && this.newMember.fname && this.newMember.lname)) {
-                alert("All fields must be filled")
+            // if (!(this.newMember.instrument && this.newMember.fname && this.newMember.lname)) {
+            if ((this.newMember.fname && this.newMember.lname && this.newMember.instrument)) {
+            //     alert("All fields must be filled")
+            this.members.push(this.newMember);
+            this.newMember = {};
             } else {
-                this.members.push(this.newMember);
-                this.newMember = {};
+                // this.members.push(this.newMember);
+                // this.newMember = {};
+                alert("All fields must be filled");
             }
         }
     }
-
 };
 
 Vue.createApp(handlingForms).mount('#app');
