e87416725dba0a69c2f243a75622b361689f320c
oscarlmjr <oscarlmjr@gmail.com>
2026-09-11 19:57:51 -0300

    secao_2 aula_13-16

---
+++ Project 3 - Todo List - Initial/scripts.js
@@ -0,0 +1,26 @@
+
+const todosApp = {
+    data() {
+        return {
+            todos: [],
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if (this.newTodo.text) {
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+            } else {
+                alert("O texto da tarefa é obrigatório.")
+            }
+        }
+    }
+};
+
+
+Vue.createApp(todosApp).mount('#app');
