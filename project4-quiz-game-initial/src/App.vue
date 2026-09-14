23655a418254ceeaf4179ccbc5cc7eeecb8c09f6
oscarlmjr <oscarlmjr@gmail.com>
2026-09-14 20:42:37 -0300

    secao_3 aula_21-26

---
+++ project4-quiz-game-initial/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Microphones can be used not only to pick up sound, but also<br>to project sound similar to a speaker</h1>
+
+    <input type="radio" name="options" value="True">
+    <label>True</label><br>
+    
+    <input type="radio" name="options" value="False">
+    <label>False</label><br>
+
+    <button class="send" type="button">Send</button>
+   </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+
+// https://opentdb.com/api.php?amount=1&category=18
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  input[type='radio']{
+    margin: 12px 4px;
+  }
+  
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+
+}
+
+
+</style>
