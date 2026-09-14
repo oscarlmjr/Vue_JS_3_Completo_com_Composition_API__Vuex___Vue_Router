8105523349a002281e87a4a7a8d0b53cf4d96146
oscarlmjr <oscarlmjr@gmail.com>
2026-09-14 16:19:13 -0300

    secao_2 aula_17-20

--- Project 3 - Todo List - Initial/scripts.js
+++ Project 3 - Todo List - Initial/scripts.js
@@ -15,11 +15,20 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 };
+                localStorage.setItem("todos", JSON.stringify(this.todos));
             } else {
                 alert("O texto da tarefa é obrigatório.")
             }
         }
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+    },
+    updated() {
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+        console.log('update')
     }
+
 };
 
 
