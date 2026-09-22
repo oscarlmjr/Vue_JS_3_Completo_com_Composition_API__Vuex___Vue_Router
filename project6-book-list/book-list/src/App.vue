2f8c546b7f4c593dba409b560b1d05c6a51dfc60
oscarlmjr <oscarlmjr@gmail.com>
2026-09-22 20:48:26 -0300

    secao_5 aula_51-52

---
+++ project6-book-list/book-list/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+// ref para primitivos: números, strings, etc
+let count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+</script>
+
+<template>
+
+  <div>
+
+    <h1>{{ count }}</h1>
+    <button @click="increment">Incrementar</button>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
